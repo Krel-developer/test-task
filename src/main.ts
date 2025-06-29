61a1d259b6c873a74cf7be0177eac9a595699d15
krel <molchanov_i_v@mail.ru>
2025-06-29 19:20:23 +0500

    clear src directory, add "@" - alias

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import '@/shared/assets/styles/index.sass'
+import App from './app/App.vue'
 
 createApp(App).mount('#app')
