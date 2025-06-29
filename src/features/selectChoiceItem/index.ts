311d01155342524c3f3d9b5401684f67f5a94ec7
krel <molchanov_i_v@mail.ru>
2025-06-29 22:24:41 +0500

    fix "any"-moment

--- src/features/selectChoiceItem/index.ts
+++ src/features/selectChoiceItem/index.ts
@@ -1,6 +1,7 @@
 import { useChoiceItemsStore } from '@/entities/choiceItems'
+import type { IItem } from '@/shared/api'
 
-export function selectChoiceItem(item: { id: number; name: string }) {
+export function selectChoiceItem(item: IItem) {
   const choiceItemsStore = useChoiceItemsStore()
 
   choiceItemsStore.setCurentItem(item.id)
--- src/features/selectUserItem/index.ts
+++ src/features/selectUserItem/index.ts
@@ -1,6 +1,7 @@
 import { useUserCartStore } from '@/entities/userCart'
+import type { IItem } from '@/shared/api'
 
-export function selectUserItem(item: any) {
+export function selectUserItem(item: IItem) {
   const userCartStore = useUserCartStore()
   if (userCartStore.getActvieItemsCount < userCartStore.getMaxActiveItems) {
     userCartStore.addActiveItem(item)
